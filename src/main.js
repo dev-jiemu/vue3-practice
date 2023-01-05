c3bd621ade448fb6e1f6dfadb077fa414ce738af
jiemu <rebianne03@naver.com>
2023-01-05 19:30:46 +0900

    2023.01.05 vue3 + vite.js - vuex - axios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
+import { store } from '/@store/index.js'
+
 import './index.css'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.esm.min.js'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+const app = createApp(App)
+app.use(store)
+app.mount('#app')
---
+++ src/modules/axios.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+export default function() {
+    const BASE_URL = 'http://localhost:8080'
+    const axiosGet = (URL, onSuccess = null, onFailed = null) => {
+        const final_URL = URL.startsWith('http') ? URL : BASE_URL + URL
+        axios.get(final_URL).then((res) => {
+          if (res.status === 2000 && res.data.rsp == 'ok') {
+              if (onSuccess) {
+                  onSuccess(res.data)
+              }
+          } else {
+              if (onFailed) {
+                  onFailed(res.data)
+              }
+          }
+        })
+    }
+
+    return {
+        axiosGet,
+    }
+}
