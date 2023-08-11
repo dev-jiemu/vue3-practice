6c87b60e0ccbcf59e843caeb2047cf83843cf20c
jiye.kim <jiye.kim@callgate.co.kr>
2023-08-11 11:30:06 +0900

    2023.08.11 vue3-practice - textarea limit 처리 연습

---
+++ src/components/TextareaLimitLength.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-container>
+        <v-textarea v-model="editText" :value="editText.slice(0, maxLength)"></v-textarea>
+    </v-container>
+</template>
+<script setup>
+
+import {ref} from "vue";
+
+const editText = ref('')
+const maxLength = 10
+
+function checkTextLength(event) {
+    if (editText.value.length >= maxLength) {
+        event.preventDefault(); // 입력 막기
+    }
+}
+
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,6 +4,7 @@ import Home from '/@components/Home.vue'
 import Admin from '/@components/Admin.vue'
 import SelectBox from "../components/SelectBox.vue"
 import SelectBoxVer2 from "/@components/SelectBoxVer2.vue";
+import TextareaLimitLength from "/@components/TextareaLimitLength.vue";
 import { defineComponent } from 'vue'
 
 const NotFound = defineComponent({
@@ -18,6 +19,7 @@ const routes = [
     { path: '/admin', name: 'admin', component: Admin },
     { path: '/select_box', name: 'select_box', component: SelectBox},
     { path: '/select_box_v2', name: 'select_box_v2', component: SelectBoxVer2},
+    { path: '/textarea_limit', name: 'textarea_limit', component: TextareaLimitLength},
     { path: '/:catchAll(.*)+', component: NotFound },
 ]
 
