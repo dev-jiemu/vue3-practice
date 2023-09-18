b6b14a0b9ab78e84aa19c71aafa660aff386df9b
jiye.kim <jiye.kim@callgate.co.kr>
2023-09-19 08:40:22 +0900

    2023.09.19 vue3-practice - date picker

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,19 @@
+<template>
+    <VueDatePicker v-model="date" :enable-time-picker="false" :format="format"></VueDatePicker>
+</template>
+<script setup>
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+import {ref} from "vue";
+
+const date = ref(new Date())
+
+const format = (date) => {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    return `${year}-${month}-${day}`;
+}
+
+</script>
