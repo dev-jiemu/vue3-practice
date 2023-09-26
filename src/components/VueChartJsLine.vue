226b5c15b20e03b9d5dabfca58cf158e55565d05
jiye.kim <jiye.kim@callgate.co.kr>
2023-09-27 08:45:59 +0900

    2023.09.27 vue3-practice - chart.js with vue

---
+++ src/components/VueChartJsLine.vue
@@ -0,0 +1,37 @@
+<template>
+    <Line :data="data"/>
+</template>
+
+<script setup>
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+} from 'chart.js'
+import { Line } from 'vue-chartjs'
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
+
+const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [40, 39, 10, 40, 39, 80, 40]
+        },
+        {
+            label: 'Data Two',
+            backgroundColor: '#2f6688',
+            data: [30, 20, 63, 88, 22, 10, 40]
+        }
+    ]
+}
+
+
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -5,6 +5,8 @@ import Admin from '/@components/Admin.vue'
 import SelectBox from "../components/SelectBox.vue"
 import SelectBoxVer2 from "/@components/SelectBoxVer2.vue";
 import TextareaLimitLength from "/@components/TextareaLimitLength.vue";
+import DatePicker from "/@components/DatePicker.vue"
+import VueChartJsLine from "/@components/VueChartJsLine.vue";
 import { defineComponent } from 'vue'
 
 const NotFound = defineComponent({
@@ -20,6 +22,8 @@ const routes = [
     { path: '/select_box', name: 'select_box', component: SelectBox},
     { path: '/select_box_v2', name: 'select_box_v2', component: SelectBoxVer2},
     { path: '/textarea_limit', name: 'textarea_limit', component: TextareaLimitLength},
+    { path: '/date_picker', name: 'date_picker', component: DatePicker },
+    { path: '/vue_chart_js', name: 'vue_chart_js', component: VueChartJsLine },
     { path: '/:catchAll(.*)+', component: NotFound },
 ]
 
