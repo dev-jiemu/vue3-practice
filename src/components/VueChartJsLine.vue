488052c3ae37f367749cddb22701313680e49a48
jiye.kim <jiye.kim@callgate.co.kr>
2023-10-05 14:57:01 +0900

    2023.10.05 vue3-practice - chart.js with vue part.2 Ref. https://stackoverflow.com/questions/76529958/how-to-update-charts-from-parent-in-vue3-with-vue-chartjs-5-2 하려던거: 특정 라인만 지목해서 ON/OFF 가능한지 해보기

--- src/components/VueChartJsLine.vue
+++ src/components/VueChartJsLine.vue
@@ -1,36 +1,83 @@
 <template>
-    <Line :data="data"/>
+    <v-card>
+        <v-card-text>
+            <div>
+                <v-btn class="ma-2" color="success" variant="outlined">Add</v-btn>
+                <v-btn class="ma-2" color="red" variant="outlined" @click="updateLines(2)">Delete</v-btn>
+            </div>
+        </v-card-text>
+    </v-card>
+    <v-card height="500px">
+        <line-chart ref="chart" :data="chartData" :options="options"/>
+    </v-card>
 </template>
 
 <script setup>
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-} from 'chart.js'
-import { Line } from 'vue-chartjs'
+import {ref} from "vue";
+import LineChart from "/@components/props/LineChart.vue"
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
-
-const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-        {
-            label: 'Data One',
-            backgroundColor: '#f87979',
-            data: [40, 39, 10, 40, 39, 80, 40]
-        },
-        {
-            label: 'Data Two',
-            backgroundColor: '#2f6688',
-            data: [30, 20, 63, 88, 22, 10, 40]
+const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+        legend: {
+            display: true,
+            position: 'left',
         }
-    ]
+    }
+}
+
+const chartData = ref({
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: initData()
+})
+
+const chart = ref(null)
+
+function initData() {
+    let dataset = new Array()
+
+    for (let i = 0; i < 3; i++) {
+        let dataObj = {}
+
+        let color = getRandomColor()
+
+        dataObj.label = 'Data ' + (i + 1)
+        dataObj.borderColor = color
+        dataObj.backgroundColor = color
+        dataObj.tension = 0.5
+        dataObj.data = generateRandomNumbers(7)
+        dataObj.hidden = false
+
+        dataset.push(dataObj)
+    }
+
+    return dataset
+}
+
+function updateLines(index) {
+    let newDataset = JSON.parse(JSON.stringify(chartData.value.datasets))
+    newDataset[index].hidden = !newDataset[index].hidden
+
+    chartData.value = {...chartData.value, datasets: newDataset}
+}
+
+function generateRandomNumbers(count) {
+    const randomNumbers = [];
+    for (let i = 0; i < count; i++) {
+        const randomNumber = Math.floor(Math.random() * 100); // 0부터 99까지의 랜덤 숫자 생성
+        randomNumbers.push(randomNumber);
+    }
+    return randomNumbers;
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 
---
+++ src/components/props/LineChart.vue
@@ -0,0 +1,29 @@
+<template>
+    <Line :data="data" :options="options" />
+</template>
+
+<script setup>
+import { Line } from "vue-chartjs";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from "chart.js";
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+const { data, options } = defineProps(["data", "options"]);
+</script>
