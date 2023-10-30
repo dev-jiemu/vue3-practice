b87ce29564dbc010038135a7d23f68b814e03124
jiye.kim <jiye.kim@callgate.co.kr>
2023-10-30 13:32:13 +0900

    2023.10.30 vue3-practice - vue router param, query

--- src/components/UrlParams.vue
+++ src/components/UrlParams.vue
@@ -13,9 +13,14 @@
 <script setup>
 
 import {useRoute} from "vue-router";
+import {watch} from "vue";
 
 const route = useRoute()
 const type = route.params.type
 const query = route.query
 
+// route param값 변할때마다 watch로 감지
+watch(() => route.params.type, (newType, oldType) => {
+    console.log('new, old: ' + newType + ', ' + oldType)
+})
 </script>
