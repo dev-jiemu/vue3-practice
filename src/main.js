478fabd0364426aff10060af1ad6234410d30c2a
jiye.kim <jiye.kim@callgate.co.kr>
2024-04-30 15:37:59 +0900

    2024.04.30 vue3-practice - Sentry 사용해보기 ㅇㅂㅇ (주석참고) * 계정을 생성하고 연동하는거고, 유료버전 무료버전 따로 있는데 얘를 내 vue 서버에 도입해도 되는진 모르겠음...

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from "pinia";
 import { store } from '/@store/index.js'
 import { router } from '/@router/router.js'
 import App from '/@/App.vue'
+// import * as Sentry from "@sentry/vue";
 import '/@/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.esm.min.js'
@@ -23,6 +24,20 @@ const vuetify = createVuetify({
     components,
     directives,
 })
+
+// Sentry.init({
+//     app,
+//     dsn: "https://[user_dsn_value]",
+//     integrations: [
+//         Sentry.browserTracingIntegration(),
+//         Sentry.replayIntegration(),
+//     ],
+//     tracingOptions: { // 하위 구성 요소를 추적하고 렌더링 프로세스에 대한 자세한 내용을 보기
+//         trackComponents: true,
+//     },
+//     attachProps: true, // 로깅을 위해 모든 Vue 구성 요소의 props를 보기
+//     tracesSampleRate: 1, // 0에서 1 사이의 숫자로 주어진 트랜잭션이 Sentry로 전송 될 확률을 제어
+// });
 
 app.use(store)
 app.use(router)
